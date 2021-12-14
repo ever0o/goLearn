8c1d85f4ee5601c758ab3e39fbe5aaa582a84f68
WangY <WangY0o@outlook.com>
2021-12-14 17:36:15 +0800

    go的常量

--- two mod/202112143.go
+++ two mod/202112143.go
@@ -1,8 +1,16 @@
 package main
 
 /*
-		常量
+	常量
+	常量是一个简单值得标识符，在程序运行的时候，不会被修改的量
+	常量中的数据类型只可以是布尔型，数字型（包含整数，浮点和复数），字符串
 */
+
+//显式类型常量的定义格式
+const demoConst string = "Alex"
+
+//隐式常量类型的定义格式
+const demoConst2 = "Bob"
 
 func main() {
 
